0eaa4d580609d0192c1dbd75220be9e479d859e3
Asier97am <asier.aranda.malagon@gmail.com>
2021-12-06 16:50:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a47juego_matematico2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a47juego_matematico2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
