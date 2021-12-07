b53932fcd6eaa607c69a8148da83270d11f9c6e3
Asier97am <asier.aranda.malagon@gmail.com>
2021-12-07 13:10:23 +0100

    casi comple

--- app/src/main/java/com/example/a47juego_matematico2/MainActivity.java
+++ app/src/main/java/com/example/a47juego_matematico2/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     public Button btComprobar, btGenerarAleatorio;
     public CheckBox check_2, check_3, check_5, check_10, check_nodiv;
     public TextView tvAlea, tvComprobacion;
-    public ImageView KO,OK;
+    public ImageView KO, OK;
 
     public int aleatorio;
 
@@ -41,15 +41,15 @@ public class MainActivity extends AppCompatActivity {
         check_10 = findViewById(R.id.check_10);
         check_nodiv = findViewById(R.id.check_nodiv);
 
-        KO=findViewById(R.id.KO);
-        OK=findViewById(R.id.OK);
+        KO = findViewById(R.id.KO);
+        OK = findViewById(R.id.OK);
 
 
     }
 
     public void btGenerarAleatorio(View view) {
-        int min = 1000;
-        int max = 2000;
+        int min = 1;
+        int max = 10;
         aleatorio = (int) (Math.random() * (max + 1 - min) + min);
         tvAlea.setText("" + aleatorio);
 
@@ -60,81 +60,86 @@ public class MainActivity extends AppCompatActivity {
         int aleatorio = Integer.parseInt(String.valueOf(tvAlea.getText()));
 
 
-        boolean esDivisible;
-        if (aleatorio % 2 == 0 || aleatorio % 3 == 0 || aleatorio % 5 == 0 || aleatorio % 10 == 0) {
-            esDivisible = true;
-        } else {
-            esDivisible = false;
-            Toast.makeText(this, "no es divisible por ninguno", Toast.LENGTH_SHORT).show();
-        }
+//        || aleatorio % 3==0 || aleatorio % 5 == 0 || aleatorio % 10 == 0
+        boolean esDivisible = true;
+
+        do {
+            if (aleatorio % 2 == 0) {
+                esDivisible = true;
+                if (check_2.isChecked() && esDivisible == true) {
+                    tvComprobacion.setText("Correcto");
+                    tvComprobacion.setTextColor(Color.GREEN);
+                    OK.setVisibility(View.VISIBLE);
+                    KO.setVisibility(View.INVISIBLE);
+                } else {
+                    tvComprobacion.setText("ERROR");
+                    tvComprobacion.setTextColor(Color.RED);
+                    OK.setVisibility(View.INVISIBLE);
+                    KO.setVisibility(View.VISIBLE);
+                }
+            }
+            if (aleatorio % 3 == 0) {
+                esDivisible = true;
+                if (check_3.isChecked() && esDivisible == true) {
+                    tvComprobacion.setText("Correcto");
+                    tvComprobacion.setTextColor(Color.GREEN);
+                    OK.setVisibility(View.VISIBLE);
+                    KO.setVisibility(View.INVISIBLE);
+                } else {
+                    tvComprobacion.setText("ERROR");
+                    tvComprobacion.setTextColor(Color.RED);
+                    OK.setVisibility(View.INVISIBLE);
+                    KO.setVisibility(View.VISIBLE);
+                }
+            }
+            if (aleatorio % 5 == 0) {
+                esDivisible = true;
+                if (check_5.isChecked() && esDivisible == true) {
+                    tvComprobacion.setText("Correcto");
+                    tvComprobacion.setTextColor(Color.GREEN);
+                    OK.setVisibility(View.VISIBLE);
+                    KO.setVisibility(View.INVISIBLE);
+                } else {
+                    tvComprobacion.setText("ERROR");
+                    tvComprobacion.setTextColor(Color.RED);
+                    OK.setVisibility(View.INVISIBLE);
+                    KO.setVisibility(View.VISIBLE);
+                }
+            }
+            if (aleatorio % 10 == 0) {
+                esDivisible = true;
+                if (check_10.isChecked() && esDivisible == true) {
+                    tvComprobacion.setText("Correcto");
+                    tvComprobacion.setTextColor(Color.GREEN);
+                    OK.setVisibility(View.VISIBLE);
+                    KO.setVisibility(View.INVISIBLE);
+                } else {
+                    tvComprobacion.setText("ERROR");
+                    tvComprobacion.setTextColor(Color.RED);
+                    OK.setVisibility(View.INVISIBLE);
+                    KO.setVisibility(View.VISIBLE);
+                }
+            }
+            if (aleatorio % 2 != 0 && aleatorio % 3 != 0 && aleatorio % 5 != 0 && aleatorio % 10 != 0) {
+                if (check_nodiv.isChecked()) {
+                    tvComprobacion.setText("Correcto");
+                    tvComprobacion.setTextColor(Color.GREEN);
+                    OK.setVisibility(View.VISIBLE);
+                    KO.setVisibility(View.INVISIBLE);
+                    Toast.makeText(this, "no es divisible entre ninguno", Toast.LENGTH_SHORT).show();
+                } else {
+                    tvComprobacion.setText("ERROR");
+                    tvComprobacion.setTextColor(Color.RED);
+                    OK.setVisibility(View.INVISIBLE);
+                    KO.setVisibility(View.VISIBLE);
+                }
+            }
+        } while (esDivisible != true);
+
 
         if (!check_nodiv.isChecked() && !check_2.isChecked() && !check_3.isChecked() && !check_5.isChecked() && !check_10.isChecked()) {
             Toast.makeText(this, "Debe escoger una de las opciones", Toast.LENGTH_SHORT).show();
-        } else {
-            if (check_nodiv.isChecked() && esDivisible == false) {
-                tvComprobacion.setText("Correcto");
-                tvComprobacion.setTextColor(Color.GREEN);
-                OK.setVisibility(View.VISIBLE);
-                KO.setVisibility(View.INVISIBLE);
-            } else {
-                tvComprobacion.setText("ERROR");
-                tvComprobacion.setTextColor(Color.RED);
-                OK.setVisibility(View.INVISIBLE);
-                KO.setVisibility(View.VISIBLE);
-            }
-            if (check_2.isChecked() && esDivisible == true) {
-                tvComprobacion.setText("Correcto");
-                tvComprobacion.setTextColor(Color.GREEN);
-                OK.setVisibility(View.VISIBLE);
-                KO.setVisibility(View.INVISIBLE);
-            } else {
-                tvComprobacion.setText("ERROR");
-                tvComprobacion.setTextColor(Color.RED);
-                OK.setVisibility(View.INVISIBLE);
-                KO.setVisibility(View.VISIBLE);
-            }
-            if (check_3.isChecked() && esDivisible == true) {
-                tvComprobacion.setText("Correcto");
-                tvComprobacion.setTextColor(Color.GREEN);
-                OK.setVisibility(View.VISIBLE);
-                KO.setVisibility(View.INVISIBLE);
-            } else {
-                tvComprobacion.setText("ERROR");
-                tvComprobacion.setTextColor(Color.RED);
-                OK.setVisibility(View.INVISIBLE);
-                KO.setVisibility(View.VISIBLE);
-            }
-            if (check_5.isChecked() && esDivisible == true) {
-                tvComprobacion.setText("Correcto");
-                tvComprobacion.setTextColor(Color.GREEN);
-                OK.setVisibility(View.VISIBLE);
-                KO.setVisibility(View.INVISIBLE);
-            } else {
-                tvComprobacion.setText("ERROR");
-                tvComprobacion.setTextColor(Color.RED);
-                OK.setVisibility(View.INVISIBLE);
-                KO.setVisibility(View.VISIBLE);
-            }
-            if (check_10.isChecked() && esDivisible == true) {
-                tvComprobacion.setText("Correcto");
-                tvComprobacion.setTextColor(Color.GREEN);
-                OK.setVisibility(View.VISIBLE);
-                KO.setVisibility(View.INVISIBLE);
-            } else {
-                tvComprobacion.setText("ERROR");
-                tvComprobacion.setTextColor(Color.RED);
-                OK.setVisibility(View.INVISIBLE);
-                KO.setVisibility(View.VISIBLE);
-            }
         }
-    }
-
-    public void mostrarKO(View view) {
-        OK.setVisibility(View.INVISIBLE);
-        KO.setVisibility(View.VISIBLE);
-    }
-    public void mostrarOK(View view) {
-        KO.setVisibility(View.INVISIBLE);
-        OK.setVisibility(View.VISIBLE);
+//
     }
 }
